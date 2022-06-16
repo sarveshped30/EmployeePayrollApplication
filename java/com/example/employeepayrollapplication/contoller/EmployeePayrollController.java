bca0b4335dc4f7d78436fa0d0e9183a3ce0c1a49
sarveshped30 <sarveshpednekar30@gmail.com>
2022-06-17 02:40:38 +0530

    Added: Employee payroll app UC2-checking code for Rest API calls

---
+++ java/com/example/employeepayrollapplication/contoller/EmployeePayrollController.java
@@ -0,0 +1,38 @@
+package com.example.employeepayrollapplication.contoller;
+
+import com.example.employeepayrollapplication.dto.EmployeePayrollDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/* Controller class with @RestController annotation which receives rest api calls
+ * and provide response in form json or xml */
+@RestController
+@RequestMapping("/employeepayrollservice")
+public class EmployeePayrollController {
+
+    @RequestMapping(value = {"", "/", "/get"}, method = RequestMethod.GET)
+    public ResponseEntity<String> getEmployeePayrollData() {
+        return new ResponseEntity<>("Get call success", HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{empId}")
+    public ResponseEntity<String> getEmployeePayrollDataById(@PathVariable("empId") int empId) {
+        return new ResponseEntity<>("Get call success for id: " + empId, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
+        return new ResponseEntity<>("Created employee payroll data for: " + employeePayrollDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{empId}")
+    public ResponseEntity<String> deleteEmployeePayrollDataById(@PathVariable("empId") int empId) {
+        return new ResponseEntity<>("Deleted employee payroll data for: " + empId, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
+        return new ResponseEntity<>("Updated employeepayroll data for: " + employeePayrollDTO, HttpStatus.OK);
+    }
+}
